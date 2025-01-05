582e17e30b60838290bebaa8e64a50ac4d383989
Aditeya <aditeya.vg@subcom.tech>
2025-01-05 13:01:23 +0530

    feat: ✨basic ppm creation

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,45 @@
 const std = @import("std");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    // Allocator setup
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    const allocator = arena.allocator();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    // Constants
+    const IMAGE_WIDTH: u32 = 256;
+    const IMAGE_HEIGHT: u32 = 256;
 
-    try bw.flush(); // don't forget to flush!
-}
+    std.debug.print("=> Starting Render\n", .{});
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    // Create/Overwrite File
+    const file = try std.fs.cwd().createFile("image.ppm", .{});
+
+    // PPM Header
+    const header = try std.fmt.allocPrint(allocator, "P3\n{d} {d}\n255\n", .{IMAGE_HEIGHT, IMAGE_HEIGHT});
+    try file.writeAll(header);
+
+    for (0..IMAGE_HEIGHT) |j| {
+        for (0..IMAGE_WIDTH) |i| {
+            const r: f32 = @as(f32, @floatFromInt(i)) / @as(f32, @floatFromInt(IMAGE_WIDTH-1));
+            const g: f32 = @as(f32, @floatFromInt(j)) / @as(f32, @floatFromInt(IMAGE_HEIGHT-1));
+            const b: f32 = 0.0;
+
+            const ir: u8 = @intFromFloat(255.999 * r);
+            const ig: u8 = @intFromFloat(255.999 * g);
+            const ib: u8 = @intFromFloat(255.999 * b);
+
+            // PPM Pixel
+            const pixel = try std.fmt.allocPrint(allocator, "{d} {d} {d}\n", .{ir, ig, ib});
+            try file.writeAll(pixel);
+        }
+
+        // std.debug.print("=> Line {d:0>3} complete\n", .{j});
+    }
+
+    // close file
+    defer file.close();
+
+    std.debug.print("=> Render Complete!\n", .{});
 }
